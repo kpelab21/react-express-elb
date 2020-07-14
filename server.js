723584e8716b3b1eed2a4d7f6fe89dd8643ef210
Kevin Elaba <kevine.@Kevins-MacBook-Pro-2.local>
2020-07-14 13:24:18 -0400

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //
 import path from 'path';
 import express from 'express';
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
