6bc43fe09c3b76ec78368e208e02bc9dfaa95ad8
Kevin Elaba <kevine.@Kevins-MacBook-Pro-2.local>
2020-09-10 12:54:18 -0400

    server.js

--- server.compiled.js
+++ server.compiled.js
@@ -8,7 +8,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 // server.js
 //
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 var app = (0, _express["default"])();
 app.use("/", _express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
 app.get('/', function (req, res) {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //
 import path from 'path';
 import express from 'express';
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use("/",express.static(path.join(__dirname, 'client', 'build')));
